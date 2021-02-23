40cc658c97eef763fc5b1dde6001bf75c8a1015e
Tim Oelkers <tim.oelkers@web.de>
2021-02-23 10:23:14 +0100

    Save model and accuracy graph

--- src/ros_ai_report/training.py
+++ src/ros_ai_report/training.py
@@ -1,13 +1,10 @@
+#!/usr/bin/env python3
+
 import torch
 import torchaudio
-import torch.nn.functional as F
+import matplotlib.pyplot as plt
 from dataset import DataSetType, DataSubSet
 from model import Model
-
-sample_rate = 8000
-batch_size = 512
-num_workers = 10
-epochs = 4
 
 def collate_fn(batch):
     tensors, targets = [], []
@@ -21,34 +18,6 @@ def collate_fn(batch):
         tensors += [waveform]
         targets += [torch.tensor(labels.index(label))]
     return pad_with_zero(tensors), torch.stack(targets)
-
-train_set = DataSubSet(DataSetType.TRAINING)
-test_set = DataSubSet(DataSetType.VALIDATION)
-
-labels = train_set.get_labels()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-transform = torchaudio.transforms.Resample(orig_freq=train_set.get_sample_rate(), new_freq=sample_rate).to(device)
-
-train_loader = torch.utils.data.DataLoader(train_set,
-    batch_size=batch_size,
-    shuffle=True,
-    collate_fn=collate_fn,
-    num_workers=num_workers,
-)
-
-test_loader = torch.utils.data.DataLoader(test_set,
-    batch_size=batch_size,
-    shuffle=True,
-    collate_fn=collate_fn,
-    num_workers=num_workers,
-)
-
-model = Model(output_size=len(labels)).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0.0001)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
-loss_fn = torch.nn.NLLLoss()
-train_losses = []
-test_losses = []
 
 def trainModel():
     model.train()
@@ -81,7 +50,51 @@ def testModel():
     percentage = 100. * correct / data_size
     print("Test set: Average loss: {:.4f}, Accuracy: {}/{}, ({:.1f}%)".format(total_loss, correct, data_size, percentage))
 
-for epoch in range(1, epochs + 1):
-    trainModel()
-    testModel()
-    scheduler.step()
+def savePlot():
+    plt.xlabel("Epoch")
+    plt.ylabel("Aerage Loss")
+    plt.plot(train_losses, label="Training Set")
+    plt.plot(test_losses, label="Test Set")
+    plt.legend()
+    plt.savefig("model_losses.svg")
+    plt.close()
+
+if __name__ == "__main__":
+    sample_rate = 8000
+    batch_size = 512
+    num_workers = 10
+    epochs = 4
+    train_set = DataSubSet(DataSetType.TRAINING)
+    test_set = DataSubSet(DataSetType.VALIDATION)
+
+    labels = train_set.get_labels()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    transform = torchaudio.transforms.Resample(orig_freq=train_set.get_sample_rate(), new_freq=sample_rate).to(device)
+
+    train_loader = torch.utils.data.DataLoader(train_set,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=collate_fn,
+        num_workers=num_workers,
+    )
+
+    test_loader = torch.utils.data.DataLoader(test_set,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=collate_fn,
+        num_workers=num_workers,
+    )
+
+    model = Model(output_size=len(labels)).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0.0001)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
+    loss_fn = torch.nn.NLLLoss()
+    train_losses = []
+    test_losses = []
+
+    for epoch in range(1, epochs + 1):
+        trainModel()
+        testModel()
+        scheduler.step()
+    torch.save(model.state_dict(), "trainedModel.pt")
+    savePlot()
