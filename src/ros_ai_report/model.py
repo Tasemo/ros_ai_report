bd207e87841974d674ac5866915913f8c955fdde
Tim Oelkers <tim.oelkers@web.de>
2021-03-11 15:25:25 +0100

    Adapt model parameters, remove scheduler, output accuracies and switch to testing subset

--- src/ros_ai_report/model.py
+++ src/ros_ai_report/model.py
@@ -15,10 +15,10 @@ class Model(nn.Module):
         self.conv3 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, stride=1)
         self.bn3 = nn.BatchNorm1d(64)
         self.pool3 = nn.MaxPool1d(4)
-        self.conv4 = nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
-        self.bn4 = nn.BatchNorm1d(64)
+        self.conv4 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, stride=1)
+        self.bn4 = nn.BatchNorm1d(128)
         self.pool4 = nn.MaxPool1d(4)
-        self.fc1 = nn.Linear(64, output_size)
+        self.fc1 = nn.Linear(128, output_size)
 
     def forward(self, x):
         x = self.conv1(x)
--- src/ros_ai_report/training.py
+++ src/ros_ai_report/training.py
@@ -23,16 +23,23 @@ def collate_fn(batch):
 def trainModel():
     model.train()
     total_loss = 0
+    correct = 0
     for batch_id, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(transform(data))
         loss = loss_fn(output.squeeze(), target)
         total_loss += loss.item()
+        prediced = output.argmax(dim=-1).squeeze()
+        correct += prediced.eq(target).sum().item()
         loss.backward()
         optimizer.step()
-    total_loss /= len(train_loader.dataset)
+    data_size = len(train_loader.dataset)
+    total_loss /= data_size
     train_losses.append(total_loss)
+    percentage = 100. * correct / data_size
+    train_accuracy.append(percentage)
+    print("Training set: Average loss: {:.4f}, Accuracy: {}/{}, ({:.1f}%)".format(total_loss, correct, data_size, percentage))
 
 def testModel():
     model.eval()
@@ -43,12 +50,13 @@ def testModel():
             data, target = data.to(device), target.to(device)
             output = model(transform(data))
             total_loss += loss_fn(output.squeeze(), target).item()
-            prediced = output.argmax().squeeze()
+            prediced = output.argmax(dim=-1).squeeze()
             correct += prediced.eq(target).sum().item()
     data_size = len(test_loader.dataset)
     total_loss /= data_size
     test_losses.append(total_loss)
     percentage = 100. * correct / data_size
+    test_accuracy.append(percentage)
     print("Test set: Average loss: {:.4f}, Accuracy: {}/{}, ({:.1f}%)".format(total_loss, correct, data_size, percentage))
 
 def savePlot():
@@ -58,6 +66,13 @@ def savePlot():
     plt.plot(test_losses, label="Test Set")
     plt.legend()
     plt.savefig("model/model_losses.svg")
+    plt.close()
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.plot(train_accuracy, label="Training Set")
+    plt.plot(test_accuracy, label="Test Set")
+    plt.legend()
+    plt.savefig("model/model_accuracies.svg")
     plt.close()
 
 def saveMetadata():
@@ -70,10 +85,10 @@ def saveMetadata():
 if __name__ == "__main__":
     sample_rate = 8000
     batch_size = 512
-    num_workers = 10
-    epochs = 5
+    num_workers = 4
+    epochs = 6
     train_set = DataSubSet(DataSetType.TRAINING)
-    test_set = DataSubSet(DataSetType.VALIDATION)
+    test_set = DataSubSet(DataSetType.TESTING)
 
     labels = train_set.get_labels()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -95,15 +110,15 @@ if __name__ == "__main__":
 
     model = Model(output_size=len(labels)).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0.0001)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
     loss_fn = torch.nn.NLLLoss()
     train_losses = []
     test_losses = []
+    train_accuracy = []
+    test_accuracy = []
 
     for epoch in range(1, epochs + 1):
         trainModel()
         testModel()
-        scheduler.step()
     torch.save(model.state_dict(), "model/trainedModel.pt")
     saveMetadata()
     savePlot()
